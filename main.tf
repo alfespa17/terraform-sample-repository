1ba601b502a4cfb2ba48fdf7ed75aa02293d728c
Alfredo España <alfespa17@gmail.com>
2021-07-16 15:05:38 -0600

    Sample terraform module

---
+++ main.tf
@@ -0,0 +1,24 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=2.46.0"
+    }
+  }
+}
+
+# Configure the Microsoft Azure Provider
+provider "azurerm" {
+  features {}
+}
+
+resource "random_uuid" "id" {
+}
+
+#resource "azurerm_resource_group" "test" {
+#  name     = "${random_uuid.id.result}-rg"
+#  location = "East US2"
+#  tags = {
+#    "sample" = var.tag_name
+#  }
+#}
